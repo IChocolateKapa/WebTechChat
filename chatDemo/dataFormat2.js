ca08edf4c4cf8604488726b27a02a6b8bc5ddddd
IChocolateKapa <echochocolatekapa@163.com>
2015-11-11 16:25:34 +0800

    +数据预处理方法

---
+++ chatDemo/dataFormat2.js
@@ -0,0 +1,585 @@
+/**
+ * Created by Echo on 2015/11/10.
+ */
+
+
+//group代表渠道来源， value代表PV值
+
+data = [
+    {
+        "name": 20151101,
+        "group": "360导航",
+        "value": 3010907
+    },
+    {
+        "name": 20151101,
+        "group": "百度品牌专区",
+        "value": 1285585
+    },
+    {
+        "name": 20151101,
+        "group": "搜狗导航",
+        "value": 668711
+    },
+    {
+        "name": 20151101,
+        "group": "2345导航",
+        "value": 644830
+    },
+    {
+        "name": 20151101,
+        "group": "金山导航",
+        "value": 440771
+    },
+    {
+        "name": 20151101,
+        "group": "QQ导航",
+        "value": 323225
+    },
+    {
+        "name": 20151101,
+        "group": "SDJZ_PC",
+        "value": 157878
+    },
+    {
+        "name": 20151101,
+        "group": "搜狗品牌专区",
+        "value": 153252
+    },
+    {
+        "name": 20151101,
+        "group": "UC导航",
+        "value": 106835
+    },
+    {
+        "name": 20151101,
+        "group": "比亚迪厂区pc",
+        "value": 105110
+    },
+    {
+        "name": 20151102,
+        "group": "360导航",
+        "value": 3857777
+    },
+    {
+        "name": 20151102,
+        "group": "百度品牌专区",
+        "value": 1600693
+    },
+    {
+        "name": 20151102,
+        "group": "搜狗导航",
+        "value": 798360
+    },
+    {
+        "name": 20151102,
+        "group": "2345导航",
+        "value": 768616
+    },
+    {
+        "name": 20151102,
+        "group": "金山导航",
+        "value": 545639
+    },
+    {
+        "name": 20151102,
+        "group": "QQ导航",
+        "value": 395408
+    },
+    {
+        "name": 20151102,
+        "group": "SDJZ_PC",
+        "value": 207977
+    },
+    {
+        "name": 20151102,
+        "group": "搜狗品牌专区",
+        "value": 190878
+    },
+    {
+        "name": 20151102,
+        "group": "比亚迪厂区pc",
+        "value": 118643
+    },
+    {
+        "name": 20151102,
+        "group": "UC导航",
+        "value": 108111
+    },
+    {
+        "name": 20151103,
+        "group": "360导航",
+        "value": 3984336
+    },
+    {
+        "name": 20151103,
+        "group": "百度品牌专区",
+        "value": 1587794
+    },
+    {
+        "name": 20151103,
+        "group": "搜狗导航",
+        "value": 812802
+    },
+    {
+        "name": 20151103,
+        "group": "2345导航",
+        "value": 783379
+    },
+    {
+        "name": 20151103,
+        "group": "金山导航",
+        "value": 550681
+    },
+    {
+        "name": 20151103,
+        "group": "QQ导航",
+        "value": 403815
+    },
+    {
+        "name": 20151103,
+        "group": "搜狗品牌专区",
+        "value": 192375
+    },
+    {
+        "name": 20151103,
+        "group": "SDJZ_PC",
+        "value": 192252
+    },
+    {
+        "name": 20151103,
+        "group": "UC导航",
+        "value": 112084
+    },
+    {
+        "name": 20151103,
+        "group": "长安工业园区PC",
+        "value": 106181
+    },
+    {
+        "name": 20151104,
+        "group": "360导航",
+        "value": 3984496
+    },
+    {
+        "name": 20151104,
+        "group": "百度品牌专区",
+        "value": 1578958
+    },
+    {
+        "name": 20151104,
+        "group": "搜狗导航",
+        "value": 821225
+    },
+    {
+        "name": 20151104,
+        "group": "2345导航",
+        "value": 787186
+    },
+    {
+        "name": 20151104,
+        "group": "金山导航",
+        "value": 536787
+    },
+    {
+        "name": 20151104,
+        "group": "QQ导航",
+        "value": 409519
+    },
+    {
+        "name": 20151104,
+        "group": "搜狗品牌专区",
+        "value": 192295
+    },
+    {
+        "name": 20151104,
+        "group": "SDJZ_PC",
+        "value": 172696
+    },
+    {
+        "name": 20151104,
+        "group": "UC导航",
+        "value": 112783
+    },
+    {
+        "name": 20151104,
+        "group": "长安工业园区PC",
+        "value": 104794
+    },
+    {
+        "name": 20151105,
+        "group": "360导航",
+        "value": 4046775
+    },
+    {
+        "name": 20151105,
+        "group": "百度品牌专区",
+        "value": 1568895
+    },
+    {
+        "name": 20151105,
+        "group": "搜狗导航",
+        "value": 822449
+    },
+    {
+        "name": 20151105,
+        "group": "2345导航",
+        "value": 768193
+    },
+    {
+        "name": 20151105,
+        "group": "金山导航",
+        "value": 543409
+    },
+    {
+        "name": 20151105,
+        "group": "QQ导航",
+        "value": 412415
+    },
+    {
+        "name": 20151105,
+        "group": "搜狗品牌专区",
+        "value": 199111
+    },
+    {
+        "name": 20151105,
+        "group": "SDJZ_PC",
+        "value": 166688
+    },
+    {
+        "name": 20151105,
+        "group": "UC导航",
+        "value": 112766
+    },
+    {
+        "name": 20151105,
+        "group": "长安工业园区PC",
+        "value": 86928
+    },
+    {
+        "name": 20151106,
+        "group": "360导航",
+        "value": 3850892
+    },
+    {
+        "name": 20151106,
+        "group": "百度品牌专区",
+        "value": 1468944
+    },
+    {
+        "name": 20151106,
+        "group": "搜狗导航",
+        "value": 800470
+    },
+    {
+        "name": 20151106,
+        "group": "2345导航",
+        "value": 746350
+    },
+    {
+        "name": 20151106,
+        "group": "金山导航",
+        "value": 522122
+    },
+    {
+        "name": 20151106,
+        "group": "QQ导航",
+        "value": 393193
+    },
+    {
+        "name": 20151106,
+        "group": "搜狗品牌专区",
+        "value": 188928
+    },
+    {
+        "name": 20151106,
+        "group": "SDJZ_PC",
+        "value": 148535
+    },
+    {
+        "name": 20151106,
+        "group": "UC导航",
+        "value": 104726
+    },
+    {
+        "name": 20151106,
+        "group": "长安工业园区PC",
+        "value": 82405
+    },
+    {
+        "name": 20151107,
+        "group": "360导航",
+        "value": 3282678
+    },
+    {
+        "name": 20151107,
+        "group": "百度品牌专区",
+        "value": 1342637
+    },
+    {
+        "name": 20151107,
+        "group": "搜狗导航",
+        "value": 714085
+    },
+    {
+        "name": 20151107,
+        "group": "2345导航",
+        "value": 668986
+    },
+    {
+        "name": 20151107,
+        "group": "金山导航",
+        "value": 460353
+    },
+    {
+        "name": 20151107,
+        "group": "QQ导航",
+        "value": 348880
+    },
+    {
+        "name": 20151107,
+        "group": "搜狗品牌专区",
+        "value": 167286
+    },
+    {
+        "name": 20151107,
+        "group": "SDJZ_PC",
+        "value": 131077
+    },
+    {
+        "name": 20151107,
+        "group": "UC导航",
+        "value": 105560
+    },
+    {
+        "name": 20151107,
+        "group": "长安工业园区PC",
+        "value": 80578
+    },
+    {
+        "name": 20151108,
+        "group": "360导航",
+        "value": 3095588
+    },
+    {
+        "name": 20151108,
+        "group": "百度品牌专区",
+        "value": 1300608
+    },
+    {
+        "name": 20151108,
+        "group": "搜狗导航",
+        "value": 686764
+    },
+    {
+        "name": 20151108,
+        "group": "2345导航",
+        "value": 637028
+    },
+    {
+        "name": 20151108,
+        "group": "金山导航",
+        "value": 437792
+    },
+    {
+        "name": 20151108,
+        "group": "QQ导航",
+        "value": 335374
+    },
+    {
+        "name": 20151108,
+        "group": "搜狗品牌专区",
+        "value": 160461
+    },
+    {
+        "name": 20151108,
+        "group": "SDJZ_PC",
+        "value": 127685
+    },
+    {
+        "name": 20151108,
+        "group": "UC导航",
+        "value": 109607
+    },
+    {
+        "name": 20151108,
+        "group": "长安工业园区PC",
+        "value": 77169
+    },
+    {
+        "name": 20151109,
+        "group": "360导航",
+        "value": 3878638
+    },
+    {
+        "name": 20151109,
+        "group": "百度品牌专区",
+        "value": 1595427
+    },
+    {
+        "name": 20151109,
+        "group": "搜狗导航",
+        "value": 794890
+    },
+    {
+        "name": 20151109,
+        "group": "2345导航",
+        "value": 749455
+    },
+    {
+        "name": 20151109,
+        "group": "金山导航",
+        "value": 529112
+    },
+    {
+        "name": 20151109,
+        "group": "QQ导航",
+        "value": 394151
+    },
+    {
+        "name": 20151109,
+        "group": "搜狗品牌专区",
+        "value": 192992
+    },
+    {
+        "name": 20151109,
+        "group": "SDJZ_PC",
+        "value": 159162
+    },
+    {
+        "name": 20151109,
+        "group": "UC导航",
+        "value": 106539
+    },
+    {
+        "name": 20151109,
+        "group": "比亚迪厂区pc",
+        "value": 88597
+    }
+];
+
+function preHandleData(data){
+    //其实做的是数据预处理，即均衡所有数据，使所有数据都有这个值
+    //I'm so brilliant!
+    var dateKey = {};
+    for(var i = 0; i < data.length; i++){
+        //获取同name的，即分别获取同一日期下的数组
+        if(!(data[i].name in dateKey)){
+            dateKey[data[i].name] = 1;
+        }
+    }
+
+    console.log("dateKey : ", dateKey);
+    var dataArr = [];
+    for(var date in dateKey){
+        var temp = {};
+        temp[date] = {};
+        for(var i = 0; i < data.length; i++){
+            if(data[i].name == date){
+                temp[date][data[i].group] = data[i];
+            }
+        }
+        dataArr.push(temp);
+    }
+
+    console.log("dataArr: ", dataArr);
+    //此时已经获得了[[1101的对象集合], [1102的对象集合], [1103的对象集合],]接下来要给原来的Data数组进行补充
+
+    var xAxis = [];
+    var group = [];
+
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < xAxis.length && xAxis[j] != data[i].name; j++);
+        if (j == xAxis.length)
+            xAxis.push(data[i].name);
+        for (var k = 0; k < group.length && group[k] != data[i].group; k++);
+        if (k == group.length)
+            group.push(data[i].group);
+    }
+
+
+    //遍历所有分类
+    for(var i = 0; i < dataArr.length; i++){
+
+        for(var j = 0; j < group.length; j++){
+
+            for(var date in dataArr[i]){
+                //for(var k = 0; k < dataArr[i][date].length; k++){
+                //for(var kk in dataArr[i][date]){
+
+                if (typeof dataArr[i][date][group[j]] == "undefined") {
+
+                    console.log("ddddd bingo! == ", dataArr[i][date])
+                    console.log("jjjjjjjjjjj bingo! == ", group[j])
+                    //for (var a in dataArr[i][date][0]){
+                    //    console.log(a)
+                    data.push({
+                        name: date,
+                        value: 0,
+                        group: group[j]
+                    })
+                    //}
+
+                }
+                //}
+            }
+
+        }
+    }
+
+    console.log(data)
+    console.log(data.length)
+
+}
+
+
+
+function preHandleData2(data){
+    //1.获得group
+
+    //其实做的是数据预处理，即均衡所有数据，使所有数据都有这个值
+    //I'm so brilliant!
+
+    var xAxis = [];
+    var group = [];
+
+    for (var i = 0; i < data.length; i++) {
+        if(xAxis.indexOf(data[i].name) == -1){
+            xAxis.push(data[i].name);
+        }
+        if(group.indexOf(data[i].group) == -1){
+            group.push(data[i].group);
+        }
+    }
+
+    var dataArr = [];
+    for(var i = 0; i < xAxis.length; i++){
+
+        //对数据进行分组
+        dataArr[xAxis[i]] = {};
+        for(var j = 0; j < data.length; j++){
+            if(data[j].name == xAxis[i]){
+                dataArr[xAxis[i]][data[j].group] = data[j];
+            }
+        }
+
+
+        for(var j = 0; j < group.length; j++){
+            if (typeof dataArr[xAxis[i]][group[j]] == "undefined") {
+                data.push({
+                    name: xAxis[i],
+                    value: 0,
+                    group: group[j]
+                })
+            }
+
+        }
+    }
+    return [xAxis, group, data];
+}
+
+var retVal = preHandleData2(data);
+
+console.log(retVal[0]);
+console.log(retVal[1]);
+console.log(retVal[2]);
