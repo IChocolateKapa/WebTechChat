805756dbab160a210b799b24aefa3fe6cad5ec45
IChocolateKapa <echochocolatekapa@163.com>
2015-11-24 18:08:40 +0800

    +server get方法

---
+++ chatDemo/server/server_get.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Echo on 2015/11/24.
+ */
+var http = require('http');
+var url = require('url');
+var util = require('util');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(util.inspect(url.parse(req.url, true)));
+}).listen(8988);
+
+console.log('Server running at http://127.0.0.1:8988/');
